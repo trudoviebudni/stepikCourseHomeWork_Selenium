6d7a604bc1df3093ba79dd691b15556b3d1aab39
trudoviebudni <mumkoo@gmail.com>
2024-04-03 01:08:54 +0300

    Added the ability to choose a webdriver

--- conftest.py
+++ conftest.py
@@ -1,12 +1,31 @@
 import pytest
 from selenium import webdriver
 
-
+# для запуска pytest -s -v --browser_name=firefox test_name.py
 # позволяет автоматически импортировать фикстуры из данного модуля в разные тесты
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+
+supported_browsers = {
+    'chrome': webdriver.Chrome,
+    'firefox': webdriver.Firefox
+}
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name in supported_browsers:
+        browser = supported_browsers.get(browser_name)()
+        print(f"\nstart {browser_name} browser for test..")
+    else:
+        joined_browsers = ', '.join(supported_browsers.keys())
+        raise pytest.UsageError(f"--browser_name is invalid, supported browsers: {joined_browsers}")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
