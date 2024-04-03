7bce5301f3917141c3d861128c441166bb1757d0
trudoviebudni <mumkoo@gmail.com>
2024-04-03 22:03:54 +0300

    Minor changes

--- module_3_testingFrameworks/test_items.py
+++ module_3_testingFrameworks/test_items.py
@@ -1,6 +1,6 @@
 import time
 
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
